50b12b01c5af5d3f72f75a71345cccdeb9a72089
Mitch (Tre) Moore III <60672637+Treterten@users.noreply.github.com>
2020-11-12 18:48:58 -0600

    Revert "Seeding function now adds an array of images (all of random lengths a…"

--- server/database-seeding/review-controller.js
+++ server/database-seeding/review-controller.js
@@ -1,9 +1,5 @@
 const { getObjectId } = require('mongo-seeding');
 
-const AWS = require('aws-sdk');
-
-const path = require('path');
-
 const desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const firstNames = ['James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer', 'Michael', 'Linda', 'William', 'Elizabeth'];
@@ -14,38 +10,27 @@ const recommended = [true, false];
 
 const documents = [];
 
-AWS.config.loadFromPath(path.resolve(__dirname, '../../config.json'));
+for (let i = 0; i < 101; i += 1) {
+  const obj = {
+    author: {
+      firstName: firstNames[Math.floor(Math.random() * firstNames.length)],
+      lastName: lastNames[Math.floor(Math.random() * lastNames.length)],
+    },
+    _id: getObjectId(i.toString()),
+    helpfuls: Math.floor(Math.random() * (1000)),
+    description: desc.substring(0, Math.floor(Math.random() * 200) + 1),
+    tagline: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    dateAdded: Date.now(),
+    recommended: recommended[Math.floor(Math.random() * 2)],
+  };
+  documents.push(obj);
+}
 
-const s3 = new AWS.S3();
-const promise = s3.listObjectsV2({ Bucket: 'fec-storage' }).promise();
+const data = [
+  {
+    model: 'review',
+    documents,
+  },
+];
 
-module.exports.getData = () => {
-  let imageURLS = [];
-  return promise.then((data) => {
-    imageURLS = data.Contents.map((image) => `https://fec-storage.s3.us-east-2.amazonaws.com/${image.Key}`);
-    for (let i = 0; i < 101; i += 1) {
-      const obj = {
-        author: {
-          firstName: firstNames[Math.floor(Math.random() * firstNames.length)],
-          lastName: lastNames[Math.floor(Math.random() * lastNames.length)],
-        },
-        _id: getObjectId(i.toString()),
-        helpfuls: Math.floor(Math.random() * (1000)),
-        description: desc.substring(0, Math.floor(Math.random() * 200) + 1),
-        tagline: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        dateAdded: Date.now(),
-        recommended: recommended[Math.floor(Math.random() * 2)],
-        images: imageURLS.slice(Math.floor(Math.random() * imageURLS.length),
-          Math.floor(Math.random() * imageURLS.length) + 1),
-      };
-      documents.push(obj);
-    }
-    const exportData = [
-      {
-        model: 'Review',
-        documents,
-      },
-    ];
-    return exportData;
-  });
-};
+module.exports = data;
--- server/database-seeding/seed.js
+++ server/database-seeding/seed.js
@@ -1,21 +1,19 @@
 const path = require('path');
 const Seeder = require('mongoose-seed');
-const reviewControl = require('./review-controller');
+const data = require('./review-controller');
 
 const db = 'mongodb://localhost:27017/reviews';
 
 Seeder.connect(db, () => {
-  reviewControl.getData().then((exportData) => {
-    Seeder.loadModels([path.resolve(__dirname, '../models/review.model.js')]);
-    Seeder.clearModels(['Review'], () => {
-      Seeder.populateModels(exportData, (err, done) => {
-        if (err) {
-          Seeder.disconnect();
-          throw err;
-        }
+  Seeder.loadModels([path.resolve(__dirname, '../models/review.model.js')]);
+  Seeder.clearModels(['review'], () => {
+    Seeder.populateModels(data, (err, done) => {
+      if (err) {
         Seeder.disconnect();
-        return done;
-      });
+        throw err;
+      }
+      Seeder.disconnect();
+      return done;
     });
   });
 });
