05520382177b9b304ebb950ed85b7a6f27365ccc
Treterten <mitch3moore@gmail.com>
2020-11-12 18:37:09 -0600

    Seeding function now adds an array of images (all of random lengths and random keys) to each review

--- server/database-seeding/seed.js
+++ server/database-seeding/seed.js
@@ -1,19 +1,21 @@
 const path = require('path');
 const Seeder = require('mongoose-seed');
-const data = require('./review-controller');
+const reviewControl = require('./review-controller');
 
 const db = 'mongodb://localhost:27017/reviews';
 
 Seeder.connect(db, () => {
-  Seeder.loadModels([path.resolve(__dirname, '../models/review.model.js')]);
-  Seeder.clearModels(['review'], () => {
-    Seeder.populateModels(data, (err, done) => {
-      if (err) {
+  reviewControl.getData().then((exportData) => {
+    Seeder.loadModels([path.resolve(__dirname, '../models/review.model.js')]);
+    Seeder.clearModels(['Review'], () => {
+      Seeder.populateModels(exportData, (err, done) => {
+        if (err) {
+          Seeder.disconnect();
+          throw err;
+        }
         Seeder.disconnect();
-        throw err;
-      }
-      Seeder.disconnect();
-      return done;
+        return done;
+      });
     });
   });
 });
