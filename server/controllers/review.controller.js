b707b3d14cfbeef974e9693572fe292f0aa05cc0
Tre Moore <mitch3moore@gmail.com>
2021-06-04 15:39:17 -0700

    Fixed DB url

--- server/controllers/review.controller.js
+++ server/controllers/review.controller.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Review = require('../models/review.model.js');
 
-mongoose.connect('mongodb://localhost/reviews');
+mongoose.connect('mongodb://100.27.26.88:27017/reviews');
 
 module.exports = {
   getReviewData: (id) => Review.find({ siteId: id }).then((reviews) => reviews),
